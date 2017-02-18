47c7dec0af859abbe12883ae3f53c5f34bc5be26
himeno-yusuke <himeno-yusuke@kayac.com>
2017-02-18 23:48:20 +0900

    モジュール分ける

--- app/scripts.babel/main.js
+++ app/scripts.babel/main.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import _ from "lodash";
 import formatSheetData from "./lib/formatSheetData";
+import calcDotArray from "./lib/calcDotArray";
 
 const API_ROOT_URL = "https://api.moves-app.com/api/1.1";
 const PLACES = "/user/places/daily/";
@@ -79,42 +80,6 @@ function getEnterOfficeTime(date) {
   const min = date.slice(11, 13);
   const sec = date.slice(13, 15);
   return `${hour}:${min}:${sec}`;
-}
-
-function calcDotArray(word, gridSize) {
-  const dotArray = [];
-  const canvas = document.createElement('canvas');
-  canvas.width = gridSize;
-  canvas.height = gridSize;
-  const cw = canvas.width;
-  const ch = canvas.height;
-
-  const context = canvas.getContext('2d');
-  context.font = `${gridSize}px "Helvetica"`;
-  context.textBaseline = 'top';
-  context.textAlign = 'left';
-  context.fillStyle = 'rgb(0, 0, 0)';
-  context.clearRect(0, 0, cw, ch);
-  for(let i = 0; i < word.length; i++) {
-    context.clearRect(0, 0, cw, ch);
-    context.fillText(word[i], 0, 0);
-    const imgData = context.getImageData(0, 0, cw, ch);
-    const pixels = imgData.data;
-    document.body.appendChild(canvas);
-
-    for(let h = 0; h < ch; h++) {
-      const row = [];
-      for (let w = 0; w < cw; w++) {
-        const colorBasePos = (w + h * cw) * 4  +3;
-        const alpha = pixels[colorBasePos];
-        row.push(alpha > 127);
-      }
-      dotArray.push(row);
-    }
-
-  }
-  console.log(dotArray);
-  return dotArray;
 }
 
 function setRectAnimation() {
