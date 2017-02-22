aac6af6304d4132ce990d300e50eee45eda0fa9b
kokubu-yumiko <kokubu-yumiko@kayac.com>
2017-02-22 15:25:49 +0900

    電飾の色いれる

--- app/scripts.babel/view.js
+++ app/scripts.babel/view.js
@@ -2,9 +2,22 @@ import $ from "jquery";
 
 const date = new Date();
 const $wrapper = $('.wrapper');
-if (date.getHours() >= 18) {
+
+const isNight = (() => {
+    if (date.getHours() >= 18) {
+        return true;
+    } else {
+        // return false;
+        return true; //debug
+    }
+})();
+
+
+if (isNight) {
     $wrapper.addClass('night');
 } else {
-    $wrapper.addClass('night');
+    $wrapper.addClass('night'); //debug
     // $wrapper.addClass('morning');
 }
+
+module.exports = isNight;
